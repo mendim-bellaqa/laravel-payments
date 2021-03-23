dcb6e47e8389c21b509d091ce0038e7be3ce0b88
mendim-bellaqa <bellaqamendim@gmail.com>
2021-03-23 11:47:27 +0100

    From db to controller

--- app/Http/Controllers/ZwController.php
+++ app/Http/Controllers/ZwController.php
@@ -9,11 +9,65 @@ use App\Models\zw;
 
 class ZwController extends Controller
 {
-   //
-   function index()
-   {
+//
+function index()
+{
+$data = Array();
+$dbconn = pg_connect("host=89.174.178.194 port=5432 dbname=hcelblag user=hcelb password=homvendo123");
+$qry = 'select * from ZW';
+$result = pg_query($dbconn, $qry);
+$results = pg_fetch_all($result);
+foreach ($results as $rs)
+{
+$zw['test1'] = $rs['tr_tr_rodzaj'];
 
-       $data= DB::select('select * from zw', array(1));
-       return view('zw',['data'=>$data]);
-   }
+$zw['data'] = $rs['m1tre0_k_value971'];
+$zw['ORDER NO'] = $rs['tr_1971859888'];
+$zw['SERIA'] = $rs['zltr_1307032275'];
+$zw['KOD KLIENTA'] = $rs['ktr_k_kod'];
+$zw['MODEL'] = $rs['m831tr_tr_value911'];
+$zw['MATERIAL'] = $rs['tr_tr_opis'];
+$zw['PROFORMA NR'] = $rs['m831tr_tr_value912'];
+$zw['PROFORMA'] = $rs['tr_1971859888'];
+$zw['STATUS HISTORY'] = $rs['hsttr_sh_opis'];
+$zw['DELIVERY PAYMENT AFTER PROFORM'] = $rs['m1tre0_k_value975'];
+$zw['ORDER VALUE [PLN]'] = $rs['tr_73104138'];
+$zw['Value proforma PLN'] = $rs['m831tr_tr_value969'];
+$zw['Value proforma Euro'] = $rs['m831tr_tr_value973'];
+$zw['ZO 1'] = $rs['m831tr_tr_value912'];
+$zw['ZO 2'] = $rs['m831tr_tr_value912'];
+$zw['PROD DATE ZO 1'] = $rs['m831tr_767507672'];
+$zw['PROD DATE ZO 2'] = $rs['m831tr_767507672']; //to check
+$zw['PAYMENT DATE PROFORMA'] = $rs['tr_tr_datasprzedaz'];
+$zw['OPEN INVOCE'] = $rs['tr_tr_rodzaj']; //to check
+$zw['Proforma'] = $rs['tr_73104138']; //to check
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+array_push($data,$zw);
+}
+
+return view('zw',['data'=>$data]);
+}
 }
--- resources/views/zw.blade.php
+++ resources/views/zw.blade.php
@@ -1,15 +1,19 @@
-
 <html>
 <head>
-<h1 style: text-align: center;  > PAYMENTS - ZW </h1>
+<!-- ... -->
+<meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link href="/tailwind.css" rel="stylesheet">
+
+<h1 style: text-align: center; > PAYMENTS - ZW </h1>
 <title>PAYMENTS - ZW</title>
 </head>
 <style>
-    h1 {  
-    text-align: center;  
-} 
+h1 {
+text-align: center;
+}
 table {
-  border-collapse: collapse;
+border-collapse: collapse;
 }
 </style>
 <body>
@@ -40,39 +44,38 @@ table {
 <td>Value 3 days before internal payment date</td>
 <td>COUNTIF - positions to pay</td>
 <td>COUNTIF - positions payed</td>
-<td>COUNTIF -  positions still to pay</td>
+<td>COUNTIF - positions still to pay</td>
 </tr>
-
 
 
 @foreach($data as $data)
 <tr>
-    <td>{{$data->LP}}</td>
-    <td>{{$data->Buying_Supplier_Kategory}}</td>
-    <td>{{$data->ORDER_NO}}</td>
-    <td>{{$data->SERIA}}</td>
-    <td>{{$data->KOD_KLIENTA}}</td>
-    <td>{{$data->MODEL}}</td>
-    <td>{{$data->MATERIAL}}</td>
-    <td>{{$data->PROFORMA_NR}}</td>
-    <td>{{$data->STATUS_HISTORY}}</td>
-    <td>{{$data->DELIVERY_PAYMENT_AFTER_PROFORMA}}</td>
-    <td>{{$data->ORDER_VALUE_PLN}}</td>
-    <td>{{$data->Value_proforma_PLN}}</td>
-    <td>{{$data->Value_proforma_Euro}}</td>
-    <td>{{$data->ZO_1}}</td>
-    <td>{{$data->ZO_2}}</td>
-    <td>{{$data->PROD_DATE_ZO_1}}</td>
-    <td>{{$data->PROD_DATE_ZO_2}}</td>
-    <td>{{$data->PAYMENT_DATE_PROFORMA}}</td>
-    <td>{{$data->OPEN_INVOCE}}</td>
-    <td>{{$data->PROFORMA}}</td>
-    <td>{{$data->Value_3_days_before_punctual}}</td>
-    <td>{{$data->Value_3_days_before_internal_payment_date}}</td>
-    <td>{{$data->PROFORMA}}</td>
-    <td>{{$data->PROFORMA}}</td>
-    <td>{{$data->PROFORMA}}</td>
-    <td>{{$data->PROFORMA}}</td>
+<td>{{ $data['test1'] }}</td>
+<td>{{ $data['data'] }}</td>
+<td>{{ $data['ORDER NO']}}</td>
+<td>{{ $data['SERIA'] }}</td>
+<td>{{ $data['KOD KLIENTA'] }}</td>
+<td>{{ $data['MODEL'] }}</td>
+<td>{{ $data['MATERIAL'] }}</td>
+<td>{{ $data['PROFORMA NR'] }}</td>
+<td>{{ $data['PROFORMA'] }}</td>
+<td>{{ $data['STATUS HISTORY'] }}</td>
+<td>{{ $data['DELIVERY PAYMENT AFTER PROFORM'] }}</td>
+<td>{{ $data['ORDER VALUE [PLN]'] }}</td>
+<td>{{ $data['Value proforma PLN'] }}</td>
+<td>{{ $data['Value proforma Euro'] }}</td>
+<td>{{ $data['ZO 1'] }}</td>
+<td>{{ $data['ZO 2'] }}</td>
+<td>{{ $data['PROD DATE ZO 1'] }}</td>
+<td>{{ $data['PROD DATE ZO 2'] }}</td>
+<td>{{ $data['PAYMENT DATE PROFORMA'] }}</td>
+<td>{{ $data['OPEN INVOCE'] }}</td>
+<td>{{ $data['Proforma'] }}</td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
+<td></td>
 </tr>
 
 @endforeach
